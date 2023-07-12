703b8361ebe119c9a009386ab8be933b913241f8
TuanNguyenDin <89063829+TuanNguyenDin@users.noreply.github.com>
2023-07-12 12:54:37 +0700

    Update paymentQR.js
    Fix error send mail

--- routes/paymentQR.js
+++ routes/paymentQR.js
@@ -12,51 +12,47 @@ const transporter = nodemailer.createTransport({
     },
   });
 
-const mailOptions = {
-  from: 'DinhTuann987161@gmail.com',
-  to: 'dinhtuann161@gmail.com',
-  subject: 'Xác nhận giao dịch',
-  html:'<h4>cảm ơn bạn đã sử dụng dịch vụ của chúng tôi</h4><p>Vui long kiểm tra lại những thông tin sau:</p> <p>Sản phẩm: {productName}</p></p> <p>Địa chỉ giao dịch: {address}</p><p>thông tin liên lạc người nhận: {phone}</p>',
-};
-
-router.post("/paymentQR",(req,res)=>{
+  const mailOptions = {
+    from: 'DinhTuann987161@gmail.com',
+    to: 'dinhtuann161@gmail.com',
+    subject: 'Xác nhận giao dịch',
+    html: '<h4>Cảm ơn bạn đã sử dụng dịch vụ của chúng tôi</h4><p>Vui lòng kiểm tra lại những thông tin sau:</p>{productList}<p>Địa chỉ giao dịch: {address}</p><p>Thông tin liên lạc người nhận: {phone}</p>',
+  };
   
-  const transactionId = req.body.transactionId;
-  mailOptions.html = mailOptions.html.replace('{transactionId}', transactionId);
+  router.post("/payment", async (req, res) => {
+    const newOrder = new Order(req.body);
+    const products = req.body.products;
+  
+    let productListHtml = '';
+    products.forEach((product, index) => {
+      productListHtml += `<p>Sản phẩm ${index + 1}: ${product.title}</p>`;
+    });
+  
+    mailOptions.html = mailOptions.html.replace('{productList}', productListHtml);
+    mailOptions.html = mailOptions.html.replace('{address}', req.body.address);
+    mailOptions.html = mailOptions.html.replace('{phone}', req.body.contact);
+  
+    mailOptions.to = req.body.email;
+  
     transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-          console.log('Error sending email:', error);
-          res.status(500)
-        } else {
-          console.log('Email sent:', info.response);
-          res.status(200)
-        }
-      });
-})
-router.post("/payment",async (req,res)=>{
-
-  const newOrder = new Order(req.body);
-
-  mailOptions.html = mailOptions.html.replace('{address}', req.body.address);
-  mailOptions.html = mailOptions.html.replace('{phone}', req.body.contact);
-  mailOptions.html = mailOptions.html.replace('{productName}', req.body.product.title);
-  mailOptions.to = req.body.email;
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-          console.log('Error sending email:', error);
-          res.status(500)
-        } else {
-          console.log('Email sent:', info.response);
-          res.status(200)
-        }
-      });
-  try {
-    const savedOrder = await newOrder.save();
-    res.status(200).json(savedOrder);
-  } catch (err) {
-    res.status(500).json(err);
-    console.log("Order err")
-  }
-})
+      if (error) {
+        console.log('Error sending email:', error);
+        res.status(500).json({ error: 'Error sending email' });
+      } else {
+        console.log('Email sent:', info.response);
+        res.status(200).json({ message: 'Email sent' });
+      }
+    });
+  
+    try {
+      const savedOrder = await newOrder.save();
+      res.status(200).json(savedOrder);
+    } catch (err) {
+      res.status(500).json(err);
+      console.log("Order error:", err);
+    }
+  });
+  
+  
 
 module.exports = router;
